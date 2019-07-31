1397d77f7c8b94445f802c3f3fa513302c5c8626
George <starcalluh@live.com>
2019-07-31 08:28:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = " Hello, Neil deGrasse Tyson!"
 
-var greetSpecialGuest = "" + ${specialGuest} + "!";
+var greetSpecialGuest = ""  + "!";
 
 var topic = "space";
 
